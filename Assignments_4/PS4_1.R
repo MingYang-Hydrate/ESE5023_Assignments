1c1fe196d6d412ef75d61be3f3181137f77389e8
ping <1106933136@qq.com>
2020-11-19 15:47:31 +0800

    胡兆平_12032344

---
+++ Assignments_4/PS4_1.R
@@ -0,0 +1,158 @@
+setwd('C:/Users/ping ping/Desktop/Assignment/Assignment_04_hzp')
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+#加载美国爱荷华州五个观测站的降水数据
+data4_1 <- read.csv('PRCP.csv',header = T)
+data4_1 <- as_tibble(data4_1)
+
+
+### Boxplot
+#逐日的降水有很多0值，因此先将其转化为逐月的降水，再绘制箱线图
+data4_11 <- data4_1 %>%
+  mutate(y_m = as.character(paste(YEAR,MONTH,15,sep = "-"))) %>%
+  mutate(group = paste(y_m,NAME)) %>%
+  group_by(group) %>%
+  summarize(PRCP_mon =  sum(PRCP,na.rm = TRUE)) %>%
+  mutate(name = factor(gsub(" ","",substr(group,11,nchar(group))))) %>%
+#https://www.imooc.com/wenda/detail/585314 学习了命令nchar(),计算字符串长度
+#https://blog.csdn.net/weixin_43718786/article/details/107614620 学习了命令gsub(),替换字符串中的指定内容
+#绘图  
+  ggplot(aes(x = name, y = PRCP_mon, fill = name)) +
+  geom_boxplot() +
+  labs(title="monthly total precipitation from 2005 to 2019", x="City", 
+       y="monthly total precipitation(mm)") +
+  theme_bw() +
+  theme(plot.title=element_text(size=20, face="bold"), 
+        axis.text.x=element_text(size=10), 
+        axis.text.y=element_text(size=10),
+        axis.title.x=element_text(size=15),
+        axis.title.y=element_text(size=15)) +
+  scale_fill_discrete(name="City")
+data4_11
+
+### Time series
+#将年、月、日组合，转化为时间
+data4_12 <- data4_1 %>%
+  mutate(DATE = as.Date(paste(YEAR,MONTH,DAY,sep = "-"))) %>%
+#绘图,由于数据较多且较为相似，因此将其绘制在多个坐标系中
+  ggplot( aes(x=DATE, y=PRCP, color=NAME) ) + 
+  geom_line() + 
+  labs(title="precipitation from 2005 to 2019", x="Date", y="precipitation(mm)") +
+  theme_bw() +
+  theme(plot.title=element_text(size=20, face="bold"), 
+        axis.text.x=element_text(size=10), 
+        axis.text.y=element_text(size=10),
+        axis.title.x=element_text(size=15),
+        axis.title.y=element_text(size=15)) + 
+  scale_fill_discrete(name="City") +
+  facet_wrap( ~ NAME)
+data4_12
+
+
+### Histogram
+#基于与箱线图相同的原因，直方图也使用逐月的降水数据
+data4_13 <- data4_1 %>%
+  mutate(y_m = as.character(paste(YEAR,MONTH,15,sep = "-"))) %>%
+  mutate(group = paste(y_m,NAME)) %>%
+  group_by(group) %>%
+  summarize(PRCP_mon =  sum(PRCP,na.rm = TRUE)) %>%
+  mutate(name = factor(gsub(" ","",substr(group,11,nchar(group))))) %>%
+#绘图
+  ggplot(aes(x = PRCP_mon, fill = name)) +
+  geom_histogram() +
+  labs(title="monthly total precipitation from 2005 to 2019", x="monthly total precipitation(mm)", 
+       y="frequency") +
+  theme_bw() +
+  theme(plot.title=element_text(size=20, face="bold"), 
+        axis.text.x=element_text(size=10), 
+        axis.text.y=element_text(size=10),
+        axis.title.x=element_text(size=15),
+        axis.title.y=element_text(size=15)) +
+  scale_fill_discrete(name="City") +
+  facet_wrap( ~ name)
+data4_13
+
+
+
+### Scatter plot 
+#散点图使用逐月的降水数据，选择城市为ROCKWELLCITY和SACCITY
+data4_14 <- data4_1 %>%
+  mutate(y_m = as.character(paste(YEAR,MONTH,15,sep = "-"))) %>%
+  mutate(group = paste(y_m,NAME)) %>%
+  group_by(group) %>%
+  summarize(PRCP_mon =  sum(PRCP,na.rm = TRUE)) %>%
+  mutate(name = factor(gsub(" ","",substr(group,11,nchar(group))))) %>%
+  select(PRCP_mon,name) %>%
+  mutate(a = 1) %>%
+  mutate(b = PRCP_mon)
+ROC <- data4_14 %>%
+  filter(name == "ROCKWELLCITY,IAUS")
+SAC <- data4_14 %>%
+  filter(name == "SACCITY,IAUS")
+a <- ROC[,1]
+a[,2] <- SAC[,1]
+colnames(a) <- c('ROC','SAC')
+lm <- lm(SAC ~ ROC, data = a)
+#绘图
+plot_point <- a %>%
+  ggplot(aes(x=ROC, y=SAC)) +
+#在ggplot中添加回归线：https://blog.csdn.net/weixin_40575651/article/details/107575012?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.compare&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.compare
+  geom_abline(slope = lm$coefficients[2], intercept = lm$coefficients[1], 
+              color = "red", size = 1, alpha = 0.5) +
+  geom_point(color="blue") +
+  labs(title="monthly total precipitation from 2005 to 2019", x="ROCKWELLCITY", 
+                   y="SACCITY") +
+  theme_bw() +
+  theme(plot.title=element_text(size=20, face="bold"), 
+        axis.text.x=element_text(size=10), 
+        axis.text.y=element_text(size=10),
+        axis.title.x=element_text(size=15),
+        axis.title.y=element_text(size=15))
+plot_point
+
+
+
+### Image plot
+#绘制今年一月份全球的ndvi（归一化植被指数），数据来源为Giovanni
+library(fields)
+library(maps) 
+library(RNetCDF)
+#加载RColorBrewer包，用于绘制绿色的渐变色：https://www.jianshu.com/p/4e6fb1269ece
+library(RColorBrewer)
+ex.nc     <- open.nc("g4.subsetted.MOD13C2_006_CMG_0_05_Deg_Monthly_NDVI.20200101.180W_90S_180E_90N.nc")
+print.nc(ex.nc)
+Lat <- var.get.nc(ex.nc, "lat")
+Lon <- var.get.nc(ex.nc, "lon")
+NDVI <- var.get.nc(ex.nc, "MOD13C2_006_CMG_0_05_Deg_Monthly_NDVI") 
+close.nc(ex.nc)
+#ndvi没有海洋的数据，此外由于MODIS范围为南北纬60°左右，因此南北极无数据
+image.plot(Lon, Lat, NDVI,
+           col=brewer.pal(7,"Greens"),
+           horizontal=T,useRaster=T,
+           legend.shrink=0.75, axis.args=list(cex.axis = 1.25), 
+           legend.width=1, legend.mar=2,
+           legend.args=list(text="ndvi",cex=1.25),           
+           xlab='',ylab='',midpoint=T, axes=F, ann=F
+)
+title(xlab="",cex.lab=1.25,font.lab=2)
+axis(1,at=pretty(Lon),tck=-0.015,lwd=2,cex.axis=1.25,font=1)
+title(ylab="",cex.lab=1.25,font.lab=2)
+axis(2,at=pretty(Lat),tck=-0.015,lwd=2,cex.axis=1.25,font=1,las=1)
+title(main=paste("global ndvi in 2020.1"),
+      cex.main=1,font.main=2)
+map('world',add=T,lwd=0.75,col="black")
+box(lwd=2)
+
+
+
+
+
+
+
+
+
+
+
+
+ 
---
+++ Assignments_4/PS4_2.R
@@ -0,0 +1,101 @@
+setwd('C:/Users/ping ping/Desktop/Assignment/Assignment_04_hzp')
+#数据缺少一部分九月的值和全部十月值，下载这些数据并补充在2281305.csv中
+data4_2 <- read.csv('2281305.csv',header = T)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(lubridate)
+library(forecast)
+#整理数据，保留日期（monthly）和月平均温度（℃）
+data4_21 <- data4_2 %>%
+  mutate(tem = as.numeric(substr(TMP,2,5))) %>%
+  filter(tem != 9999) %>%
+  mutate(tem = 0.1*tem) %>%
+  mutate(date = substr(DATE,1,7) ) %>%
+  select(date,tem) %>%
+  group_by(date) %>%
+  summarize(monthly_tem = mean(tem))
+
+#4.2.1 Construct a time series
+TMP <- ts(data4_21$monthly_tem, start=c(2010,1),end = c(2020,8), frequency=12)
+plot(TMP)
+
+#4.2.2 Decompose the time series
+TMP_components <- decompose(TMP)
+plot(TMP_components)
+# 绘制直方图
+hist(TMP_components$random, prob=TRUE,ylim = c(0,0.4))
+# 添加pdf
+curve(dnorm(x, mean=mean(TMP_components$random,na.rm=T),
+            sd=sd(TMP_components$random,na.rm=T)),
+      add=TRUE, col="red")
+#Box.test : https://blog.csdn.net/qq_36810398/article/details/88876429
+Box.test(TMP_components$random,type='Ljung-Box')
+# p-value = 0.01904 < 0.05, 遵循高斯白噪声分布
+
+#4.2.3 ARIMA model
+acf(TMP)
+pacf(TMP)
+#可以发现TMP是平稳的,可以不进行差分
+
+#利用auto.arima()建模
+model <- auto.arima(TMP)
+model
+#得到的模型为ARIMA(0,0,2)(1,1,1)[12]
+
+#该模型的推导过程如下，推导过程与王超同学进行了讨论
+
+#第一组参数(0,0,2)代表去除季节性的ARIMA模型，是满足p = 0:5,q = 0:5,d = 0:2的条件下获得所有模型中，AIC最小的那个,可以通过以下方法验证
+#AIC最小的原则来自于知乎，但页面关闭后无法找回原网址了
+#p，q取值均为0:5;由于TMP是平稳的，因此d = 0:2,否则要根据差分情况确定d的取值
+TMP_adjust <- TMP - TMP_components$seasonal
+aic <- matrix(NA,6*3*6,2)
+n =1
+for (p in 0:5) {
+  for (d in 0:2) {
+    for (q in 0:5) {
+      model1 <- arima(TMP_adjust,order = c(p,d,q))
+      aic[n,1] = model1$aic
+      aic[n,2] = paste(p,d,q)
+      n = n+1
+    }
+  }
+}
+min_aic <- which(aic[,1]==min(aic[,1]))
+aic[min_aic,]
+
+
+#第二组参数(1,1,1)[12]代表季节性，是一个周期性循环的时间序列，可以通过以下方法验证
+#参考了https://blog.csdn.net/tantaixf/article/details/83148901
+#已知季节性的周期为12，因此对原数据进行差分，间隔为12
+TMP_components$seasonal
+TMP1 <- diff(TMP,12)
+acf(TMP1)
+pacf(TMP1)
+#通过肉眼可以看出，P=1,Q=1，diff()只进行了一次，因此D=1
+
+#4.2.4 Predict
+months_forecast  <- 2
+months_in_plot   <- 12
+forecast <- forecast(model, months_forecast)
+forecast
+
+# Plot predictions along with real values
+plot(forecast(model, months_forecast), include = months_in_plot , xlab="Time", 
+     ylab="temperature",type="b",lwd=2) 
+
+
+# Sep:
+# Predicted value: 29.042 (27.607, 30.478)
+# Real value: 28.800
+# Relative bias: 
+(29.042-28.800)/28.800*100
+
+#Oct
+# Predicted value: 26.401 (24.935, 27.868)
+# Real value: 25.764
+# Relative bias: 
+(26.401-25.764)/25.764*100
+
+
+
